b77e94f6c07f3704635ca641bb3633a483578aa2
Computer Science for Everyone <hughjosephnguyen@outlook.com>
2025-09-21 17:42:50 -0400

    Removed production vesion of TanStack Query dev tool

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
